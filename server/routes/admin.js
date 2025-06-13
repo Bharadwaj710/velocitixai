7fcdf379fb8367ea4a269e6c42f15adcf009d0c4
bharadwaj <bharadwaj10flasmvp@gmail.com>
2025-06-13 17:11:30 +0530

    AdminDashnoard integration

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers, getAllStudents, getAllColleges,getAllHRs } = require('../controller/adminController');
+const { getRecentNotifications, getAllNotifications, markAsRead,getAllUsers, getAllStudents, getAllColleges, getAllHRs, getStudentFilters, getHRFilters, getAdminProfile,updateAdminProfile, deleteAdminProfile} = require('../controller/adminController');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multerConfig');
 
 router.get('/users', getAllUsers);
 router.get('/students', getAllStudents);
 router.get('/colleges', getAllColleges);
 router.get('/hrs', getAllHRs);
+router.get('/students/filters', getStudentFilters);
+router.get('/hrs/filters', getHRFilters);
+router.get('/notifications/recent', getRecentNotifications);
+router.get('/notifications/all', getAllNotifications);
+router.put('/notifications/:id/read', markAsRead);
 
+// ✅ Profile Settings Routes
+router.get('/profile',auth, getAdminProfile);
+router.put('/profile', auth, multer.single('image'), updateAdminProfile);
+router.delete('/profile', auth, deleteAdminProfile);
 module.exports = router;
