a942af568e4e4ac7a32d752e792f0195ffa5f9c2
Mark Rennie <rennie0407@gmail.com>
2025-08-05 01:21:42 +0530

    added transcripts

--- server/routes/careerAssessment.js
+++ server/routes/careerAssessment.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const path = require("path");
-const CareerAssessment = require("../models/careerAssessment");
+const CareerAssessment = require("../models/CareerAssessment");
 const User = require("../models/User");
 const { videoStorage } = require("../utlis/cloudinary");
 
